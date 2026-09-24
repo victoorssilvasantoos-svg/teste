7ab2142abcb2656509b3a0feb59ad6ad809754d5
João Serra <victoorssilva.santoos@gmail.com>
2026-09-24 11:20:55 -0300

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,28 @@
+print("=" * 40)
+print("        PORTFÓLIO - JOÃO SERRA")
+print("=" * 40)
+
+print("\nSOBRE MIM")
+print("Nome: João Serra")
+print("Curso: Ciência da Computação")
+print("Área de estudo: Programação")
+
+print("\nTECNOLOGIAS UTILIZADAS")
+print("- Python")
+print("- Visual Studio Code")
+print("- Git")
+print("- GitHub")
+
+print("\nSOBRE O PORTFÓLIO")
+print("Este é meu portfólio pessoal.")
+print("Aqui apresento meus projetos e minha evolução")
+print("durante os estudos de Ciência da Computação.")
+
+print("\nOBJETIVO")
+print("Continuar estudando programação,")
+print("desenvolver novos projetos e adquirir")
+print("mais experiência na área de tecnologia.")
+
+print("\n" + "=" * 40)
+print("Obrigado por visitar meu portfólio!")
+print("=" * 40)
